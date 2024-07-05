43f87df298f7044b0360e83485f5d5fb8a0f9247
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-05 15:14:43 +0200

    [UPD] Shared header and footer

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="container-big">
+      <div class="mc-row">
+        <div class="col-33">
+          <div class="categories">
+            <a href="">Donna</a>
+            <a href="">Uomo</a>
+            <a href="">Bambini</a>
+          </div>
+        </div>
+        <div class="col-33 text-center">
+          <a href="">
+            <img class="logo" src="/img/boolean-logo.png" alt="logo" />
+          </a>
+        </div>
+        <div class="col-33">
+          <div class="container-icon">
+            <img class="icon" src="/img/user.png" alt="user" />
+            <img class="icon" src="/img/like.png" alt="like" />
+            <img class="icon" src="/img/shopping-bag.png" alt="shop" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
