92d389772d930ac6fe3ef7f0679b3ce417e15311
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-07-05 17:55:46 +0200

    [FIX] Fixed discount labels and calc price

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,27 +3,46 @@
 
 @extends('layouts.app')
 
-@section('main')
+@php
+function calcDiscount($price, $discount) {
+    if ($discount > 0) {
+        $discountedPrice = $price - ($price * $discount / 100);
+        // Arrotonda verso il basso e formatta il prezzo con due decimali
+        $discountedPrice = number_format(floor($discountedPrice * 100) / 100, 2);
+        return $discountedPrice;
+    } else {
+        return number_format($price, 2); // Se non c'è sconto, restituisci il prezzo originale formattato
+    }
+}
+@endphp
 
-<div class="mc-row">
+@section('main')
+<div class="container-big">
+    <div class="mc-row">
 
     @foreach ($data['products'] as $product)
+    @php
+        $discount = $product['discount'] ?? 0;
+    @endphp
     <div class="col-33">
         <div class="card-content">
             <!-- IMAGES -->
             <div class="img-content">
                 <img class="main-pic" src="{{ Vite::asset('resources' . $product['image']) }}" alt="{{$product['image']}}" />
-                <img class="hover-pic" :src="imageAlt" alt="{{$product['imageAlt']}}" />
+                <img class="hover-pic" src="{{ Vite::asset('resources' . $product['imageAlt']) }}" alt="{{$product['imageAlt']}}" />
                 <!-- LABELS -->
                 <div class="labels">
-                    <span class="discount-label" v-show="discount">
-                        {{-- {{ `-${discount}%` }} --}}
-                        {{-- {{ $product['discount'] }} --}}
-                    </span>
-                    <span class="sus-label" v-show="sustainability"> Sostenibilità </span>
+                    @if ($discount>0)
+                        <span class="discount-label">
+                            {{ '-'. $discount .'%' }}
+                        </span>
+                    @endif
+                    @if ( $product['sustainability'])
+                        <span class="sus-label"> Sostenibilità </span>
+                    @endif
                 </div>
                 <div
-                :class="{ liked: this.like }"
+                :class="[ 'liked'=> $product['like'] ]"
                 class="hearts-label"
                 @click="likeButton(like)">
                 &hearts;
@@ -34,13 +53,16 @@
             <div class="brand">{{ $product['brand'] }}</div>
             <div class="item">{{ $product['name'] }}</div>
             <div class="price">
-                {{-- {{ calcDiscount($product['price'], $product['discount']) }} &euro; --}}
-                <span class="discount-txt" v-show="discount">{{ $product['price'] }} &euro;</span>
+                {{ calcDiscount($product['price'], $discount) }} &euro;
+                @if ($discount > 0)
+                    <span class="discount-txt">{{ $product['price'] }} &euro;</span>
+                @endif
             </div>
         </div>
     </div>
     </div>
     @endforeach
+</div>
 </div>
 
 @endsection
